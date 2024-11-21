8d22b7b1d68957b4b87f3387523b9a999b916944
yigit433 <yigit433.devs@gmail.com>
2024-11-21 08:12:00 +0300

    update(DOCS): Python installation documentation has been updated.

---
+++ src/theme/MDXComponents.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactPlayer from 'react-player';
+
+import Tabs from '@theme/Tabs';
+import TabItem from '@theme/TabItem';
+
+import MDXComponents from '@theme-original/MDXComponents';
+
+import DownloadFileFeature from '../components/DownloadFileFeature';
+
+export default {
+    ...MDXComponents,
+    Tabs, 
+    TabItem,
+    ReactPlayer,
+    DownloadFileFeature
+};
